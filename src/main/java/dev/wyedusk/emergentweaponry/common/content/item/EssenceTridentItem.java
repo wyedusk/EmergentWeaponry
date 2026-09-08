4f3e988c6f78c14281c03303ee7287d5efbd0603
Dusk <wyedusk+github@gmail.com>
2026-09-08 23:12:42 +0100

    trident specials descriptions

--- src/main/java/dev/wyedusk/emergentweaponry/common/content/item/EssenceTridentItem.java
+++ src/main/java/dev/wyedusk/emergentweaponry/common/content/item/EssenceTridentItem.java
@@ -1,12 +1,20 @@
 package dev.wyedusk.emergentweaponry.common.content.item;
 
+import dev.wyedusk.emergentweaponry.common.EmergentWeaponry;
+import dev.wyedusk.emergentweaponry.common.config.ServerConfig;
 import dev.wyedusk.emergentweaponry.common.content.Contents;
 import dev.wyedusk.emergentweaponry.common.content.entity.ThrownEssenceTrident;
 import dev.wyedusk.emergentweaponry.common.content.entity.base.BaseThrownTrident;
 import dev.wyedusk.emergentweaponry.common.content.item.base.BaseTridentItem;
 import dev.wyedusk.emergentweaponry.common.util.PlayerUtil;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Position;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
@@ -16,12 +24,17 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.ToIntFunction;
 
 public class EssenceTridentItem extends BaseTridentItem {
@@ -41,6 +54,29 @@ public class EssenceTridentItem extends BaseTridentItem {
                 .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 8.0F, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.9F, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .build();
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
+        String itemId = Contents.Items.ESSENCE_TRIDENT.getId().getPath();
+        tooltipComponents.add(Component.translatable("tooltip." + EmergentWeaponry.MODID + "." + itemId + ".attack_special",
+                ServerConfig.ESSENCE_TRIDENT_LIFESTEAL_PERCENTAGE.getAsInt()).withStyle(ChatFormatting.DARK_GRAY));
+
+        HolderLookup.Provider registries = context.registries();
+        if (registries != null) {
+            HolderLookup.RegistryLookup<Enchantment> enchantmentRegistry = registries.lookupOrThrow(Registries.ENCHANTMENT);
+            Optional<Holder.Reference<Enchantment>> riptideEnchant = enchantmentRegistry.get(Enchantments.RIPTIDE);
+            if (riptideEnchant.isPresent()) {
+                ItemEnchantments enchantments = stack.get(DataComponents.ENCHANTMENTS);
+                if (enchantments != null) {
+                    int riptideLevel = enchantments.getLevel(riptideEnchant.get().getDelegate());
+                    if (riptideLevel > 0)
+                        tooltipComponents.add(Component.translatable("tooltip." + EmergentWeaponry.MODID + "." + itemId + ".riptide_special").withStyle(ChatFormatting.DARK_GRAY));
+                }
+            }
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
     public static Tool createToolProperties() {
--- src/main/java/dev/wyedusk/emergentweaponry/common/content/item/FrostTridentItem.java
+++ src/main/java/dev/wyedusk/emergentweaponry/common/content/item/FrostTridentItem.java
@@ -1,11 +1,18 @@
 package dev.wyedusk.emergentweaponry.common.content.item;
 
+import dev.wyedusk.emergentweaponry.common.EmergentWeaponry;
 import dev.wyedusk.emergentweaponry.common.content.Contents;
 import dev.wyedusk.emergentweaponry.common.content.entity.ThrownFrostTrident;
 import dev.wyedusk.emergentweaponry.common.content.entity.base.BaseThrownTrident;
 import dev.wyedusk.emergentweaponry.common.content.item.base.BaseTridentItem;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Position;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,12 +22,17 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Optional;
 
 public class FrostTridentItem extends BaseTridentItem {
     public FrostTridentItem(Properties properties) {
@@ -32,6 +44,28 @@ public class FrostTridentItem extends BaseTridentItem {
                 .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 8.0F, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.9F, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
                 .build();
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
+        String itemId = Contents.Items.FROST_TRIDENT.getId().getPath();
+        tooltipComponents.add(Component.translatable("tooltip." + EmergentWeaponry.MODID + "." + itemId + ".attack_special").withStyle(ChatFormatting.DARK_GRAY));
+
+        HolderLookup.Provider registries = context.registries();
+        if (registries != null) {
+            HolderLookup.RegistryLookup<Enchantment> enchantmentRegistry = registries.lookupOrThrow(Registries.ENCHANTMENT);
+            Optional<Holder.Reference<Enchantment>> riptideEnchant = enchantmentRegistry.get(Enchantments.RIPTIDE);
+            if (riptideEnchant.isPresent()) {
+                ItemEnchantments enchantments = stack.get(DataComponents.ENCHANTMENTS);
+                if (enchantments != null) {
+                    int riptideLevel = enchantments.getLevel(riptideEnchant.get().getDelegate());
+                    if (riptideLevel > 0)
+                        tooltipComponents.add(Component.translatable("tooltip." + EmergentWeaponry.MODID + "." + itemId + ".riptide_special").withStyle(ChatFormatting.DARK_GRAY));
+                }
+            }
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
     public static Tool createToolProperties() {
